c33aaa352d12917e879cf25fc9e6bbdad51eec41
NoahRametsteiner <77678379+NoahRametsteiner@users.noreply.github.com>
2022-10-03 19:04:22 +0200

    init

---
+++ Zigzag Conversion/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Zigzag_Conversion {
+    class Zigzag_Conversion {
+        static void Main(string[] args) {
+
+            Console.WriteLine(zigzag("PAYPALISHIRING",4));
+        }
+
+        public static string zigzag(string s, int numRows) {
+                string str="";
+                int colums = 0, strlen=s.Length;
+                
+                for (int b = 0;; b++) {
+                    if(strlen>numRows){
+                        strlen=strlen-numRows;
+                        colums++;
+                        for (int k = 0; k < numRows-2; k++) {
+                            if(strlen!=0){
+                                strlen--;
+                                colums++;
+                            }else{break;}
+                        }
+                    }else{
+                        if(strlen!=0){
+                            colums++;
+                        }break;
+                    }
+                }
+                
+                string[,] Numbers = new string[colums,numRows];
+                int matrixcolums = 0,strposition=0,rowposition=0;strlen=s.Length;
+                for (int b = 0;; b++) {
+                    if(strlen>numRows){
+                        strlen=strlen-numRows;
+                        for(int i=0;i<numRows;i++){Numbers[matrixcolums,i] = s[strposition].ToString();strposition++;}
+                        matrixcolums++;rowposition=numRows-2;
+                        for (int k = 0; k < numRows-2; k++) {
+                            if(strlen!=0){
+                                strlen--;
+                                Numbers[matrixcolums,rowposition] = s[strposition].ToString();strposition++;
+                                rowposition--;
+                                matrixcolums++;
+                            }else{break;}
+                        }
+                    }else{
+                        if(strlen!=0){
+                            for(int i=0;i<strlen;i++){Numbers[matrixcolums,i] = s[strposition].ToString();strposition++;}
+                            matrixcolums++;
+                        }break;
+                    }
+                }
+
+                for (int a = 0; a < Numbers.GetLength(1); a++)
+                {
+                    for (int b = 0; b < Numbers.GetLength(0); b++)
+                    {
+                        str = str + Numbers[b,a];
+                    } 
+                }
+                return str;
+            }
+    }
+}
---
+++ Zigzag Conversion/README.md
@@ -0,0 +1,5 @@
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+
+![Projekt Raycast Game.png](https://raw.githubusercontent.com/NoahRametsteiner/Software-Engineering/main/Zigzag%20Conversion/README.png)
+
+And then read line by line: "PAHNAPLSIIGYIR"
