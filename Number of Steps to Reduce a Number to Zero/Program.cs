004c2e8800933d7392f94c632f081d81f872881b
NoahRametsteiner <77678379+NoahRametsteiner@users.noreply.github.com>
2022-10-05 17:03:45 +0200

    leetcode

---
+++ Number of Steps to Reduce a Number to Zero/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Number_of_Steps_to_Reduce_a_Number_to_Zero {
+    class Number_of_Steps_to_Reduce_a_Number_to_Zero {
+        static void Main(string[] args) {
+            Console.WriteLine(nos(8));
+        }
+        public static int nos(int num) {
+            int count = 0, newnumber = num;
+
+            for(int i = 0;; i++) {
+                if(newnumber <= 0){
+                    break;
+                }
+
+                if (newnumber % 2 == 0){
+                    //Even
+                    newnumber=newnumber/2;
+                    count++;
+                }else{
+                    //Odd
+                    newnumber=newnumber-1;
+                    count++;
+                }
+            }return count;
+        }
+    }
+}
---
+++ Ransom Note/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ransom_Note {
+    class Ransom_Note {
+        static void Main(string[] args) {
+
+            Console.WriteLine(CanConstruct("aa","aab"));
+        }
+        public static bool CanConstruct(string ransomNote, string magazine) {
+            int a= 0, strlen= ransomNote.Length;
+            char[]? charNote = ransomNote.ToCharArray();
+            char[]? charmagazine = magazine.ToCharArray();
+                        
+            for (int i=0; i<strlen; i++) {
+                string str = new string(charmagazine);
+                for (int j=0; j<strlen; j++) {
+                    str = new string(charmagazine);
+                    if(str.IndexOf(charNote[i])!=-1){
+                        charmagazine[magazine.IndexOf(charNote[i])]=' ';
+                        a++;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
