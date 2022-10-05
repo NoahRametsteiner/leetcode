8d2127321b9b76d77ccb0156c82dad8cb4f5bc80
NoahRametsteiner <77678379+NoahRametsteiner@users.noreply.github.com>
2022-10-05 17:05:56 +0200

    Ransom Note

--- Ransom Note/Program.cs
+++ Ransom Note/Program.cs
@@ -2,28 +2,28 @@
 
 namespace Ransom_Note {
     class Ransom_Note {
-        static void Main(string[] args) {
-
-            Console.WriteLine(CanConstruct("aa","aab"));
+        static void Main(string[] args) {      
+            Console.WriteLine(CanConstruct("aa","aab").ToString());
         }
+
         public static bool CanConstruct(string ransomNote, string magazine) {
-            int a= 0, strlen= ransomNote.Length;
-            char[]? charNote = ransomNote.ToCharArray();
-            char[]? charmagazine = magazine.ToCharArray();
-                        
-            for (int i=0; i<strlen; i++) {
-                string str = new string(charmagazine);
-                for (int j=0; j<strlen; j++) {
-                    str = new string(charmagazine);
-                    if(str.IndexOf(charNote[i])!=-1){
-                        charmagazine[magazine.IndexOf(charNote[i])]=' ';
-                        a++;
-                    }else{
-                        return false;
+            char[] charransomNote = ransomNote.ToCharArray(); 
+            char[] charmagazine = magazine.ToCharArray();
+            int count = 0;
+
+            for(int i = 0; i < charransomNote.Length; i++) {
+                for(int j = 0; j < charmagazine.Length; j++) {
+                    if(charransomNote[i] == charmagazine[j]) {
+                        charmagazine[j] = ' ';
+                        count++;
+                        break;
                     }
                 }
             }
-            return true;
+            if(count == charransomNote.Length) {
+                return true;
+            }
+            return false;
         }
     }
 }
